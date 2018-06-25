4cf442774c7de450376688a69791c836b7b79ba4
Ryan Donoghue <duckmandrakerd@gmail.com>
2018-06-25 14:38:38 +0100

    First 5 labs completed

---
+++ CSE_2018/InvoiceApplication.java
@@ -0,0 +1,20 @@
+public class InvoiceApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double subtotal = 100;
+		double discountPercent = 0.2;
+		double discountAmount = subtotal * discountPercent;
+		double total = subtotal - discountAmount;
+		
+		System.out.println("Welcome to the Invoice Total Calculator");
+		System.out.println();
+		
+		System.out.println("Subtotal:			" + subtotal );
+		System.out.println("Discount percent:	" + discountPercent);
+		System.out.println("Discount amount: 	" + discountAmount);
+		System.out.println("Total:				" + total);
+	}
+
+}
---
+++ CSE_2018/InvoiceApplicationV1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class InvoiceApplicationV1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter subtotal:");
+		double subtotal = input.nextDouble();
+		
+		//double subtotal = 100;
+		
+		System.out.println("Enter discountPercent:");
+		double discountPercent = input.nextDouble();
+		
+		//double discountPercent = 0.2;
+		
+		double discountAmount = subtotal * discountPercent;
+		double total = subtotal - discountAmount;
+		
+		System.out.println("Welcome to the Invoice Total Calculator");
+		System.out.println();
+		
+		System.out.println("Subtotal:			" + subtotal );
+		System.out.println("Discount percent:	" + discountPercent);
+		System.out.println("Discount amount: 	" + discountAmount);
+		System.out.println("Total:				" + total);
+		
+		input.close();
+	}
+
+}
